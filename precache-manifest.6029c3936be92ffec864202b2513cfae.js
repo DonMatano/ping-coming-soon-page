953b09f102d3d98b4e9e7abb85cef29175a23dda
Matano <mwakimamatano@gmail.com>
2021-07-09 21:19:54 +0300

    deploy

---
+++ precache-manifest.6029c3936be92ffec864202b2513cfae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd057511209225ad4e3eed1d0a6057de",
+    "url": "/ping-coming-soon-page/Screenshot-2.png"
+  },
+  {
+    "revision": "69a12f02cf2de0eed0f6fb4a86527e2e",
+    "url": "/ping-coming-soon-page/Screenshot-3.png"
+  },
+  {
+    "revision": "59e29855b2dba4e0bfd9",
+    "url": "/ping-coming-soon-page/css/app.a5691987.css"
+  },
+  {
+    "revision": "92dd5c8fe5e8dde945a6f01abca7e7cc",
+    "url": "/ping-coming-soon-page/img/illustration-dashboard.92dd5c8f.png"
+  },
+  {
+    "revision": "e392d534418b8f8c25ecb73731147704",
+    "url": "/ping-coming-soon-page/img/logo.e392d534.svg"
+  },
+  {
+    "revision": "5167aff947eed88c3aeacd75249b0ffe",
+    "url": "/ping-coming-soon-page/index.html"
+  },
+  {
+    "revision": "59e29855b2dba4e0bfd9",
+    "url": "/ping-coming-soon-page/js/app.c45a0d1b.js"
+  },
+  {
+    "revision": "766f248a054fca7491be",
+    "url": "/ping-coming-soon-page/js/chunk-vendors.e622ae82.js"
+  },
+  {
+    "revision": "a8048a3ed18c7d85b15fddcab5e1174a",
+    "url": "/ping-coming-soon-page/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ping-coming-soon-page/robots.txt"
+  },
+  {
+    "revision": "02d3cf72cfb629dcff84fb83ccd2bfef",
+    "url": "/ping-coming-soon-page/screenshot-1.png"
+  },
+  {
+    "revision": "4f71a9e52d489099710dd076a0b3c70d",
+    "url": "/ping-coming-soon-page/screenshot-4-mobile.png"
+  }
+]);
